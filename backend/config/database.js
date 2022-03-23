5dcb38a252c6458b349b09634b745213a66bb4b4
md-abdul-ahammed <shagarmd2082@gmail.com>
2022-03-23 14:26:26 +0600

    fix-some

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ const connectDatabase = () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      poolSize: 20,
+      poolSize: 1,
     })
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
